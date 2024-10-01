e2d7594a69e9b941ced4eb42ff518d382d84ecff
Deepak Rawat <rwtroshan28@gmail.com>
2024-10-01 19:42:17 +0530

    remove the error

--- src/components/team/team-v3-area.tsx
+++ src/components/team/team-v3-area.tsx
@@ -10,7 +10,7 @@ const TeamThreeArea = () => {
       <div className="container details-meta">
         <div className="position-relative">
         <div className="title-one wow fadeInUp details-meta text-center mb-60">
-            <h2>Hear a Doctor's Perspective</h2>
+            <h2>Hear a Doctor&#39;s Perspective</h2>
           </div>
           {/* <p className="text-lg text-dark mb-30 lg-mb-10 text-center px-5 py-3">
             Our Team at CGB Solutions is a dynamic ensemble of individuals dedicated to excellence and innovation in workplace solutions.  United by a shared commitment to pushing boundaries and delivering exceptional results, our team fosters a culture of collaboration, continuous learning, and growth. Together, we are shaping the future of work and redefining possibilities in workplace innovation.
